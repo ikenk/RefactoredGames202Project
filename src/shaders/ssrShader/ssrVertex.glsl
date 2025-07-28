e3cf183899ebc1d52512788ac27539b9f851092b
HT_Zhang <developer_zht@163.com>
2025-07-28 17:37:10 +0800

    style: 使用Prettier格式化代码

--- src/shaders/ssrShader/ssrVertex.glsl
+++ src/shaders/ssrShader/ssrVertex.glsl
@@ -5,14 +5,12 @@ attribute vec2 aTextureCoord;
 uniform mat4 uModelMatrix;
 uniform mat4 uViewMatrix;
 uniform mat4 uProjectionMatrix;
-uniform mat4 uLightMVP; 
-
+uniform mat4 uLightMVP;
 
 varying mat4 vWorldToScreen;
 varying highp vec4 vPosWorld;
 
-
-void main(void) {
+void main(void ) {
   vec4 posWorld = uModelMatrix * vec4(aVertexPosition, 1.0);
   vPosWorld = posWorld.xyzw / posWorld.w;
   vWorldToScreen = uProjectionMatrix * uViewMatrix;
