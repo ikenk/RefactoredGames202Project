9c2d847a4768d4f5552643de113deda013a3c859
HT_Zhang <developer_zht@163.com>
2025-08-02 23:18:55 +0800

    refactor: 修改了CameraType/SceneType/LightType的数据结构
    - 从type xxx = '' | ''的类型改成了enum类型

--- src/types/engine.d.ts
+++ src/types/engine.d.ts
@@ -1,8 +1,18 @@
 import type { Vec3 } from './math'
 
-export type CameraType = 'CubeSceneCamera' | 'CaveSceneCamera'
-export type SceneType = 'CubeScene' | 'CaveScene'
-export type LightType = 'CubeLight' | 'CaveLight'
+export enum CameraType {
+  CUBE_SCENE_CAMERA = 'CubeSceneCamera',
+  CAVE_SCENE_CAMERA = 'CaveSceneCamera',
+  WATER_SCENE_CAMERA = 'WaterSceneCamera'
+}
+export enum SceneType {
+  CUBE_SCENE = 'CubeScene',
+  CAVE_SCENE = 'CaveScene'
+}
+export enum LightType {
+  CUBE_LIGHT = 'CubeLight',
+  CAVE_LIGHT = 'CaveLight'
+}
 
 declare module 'three' {
   interface PerspectiveCamera {
